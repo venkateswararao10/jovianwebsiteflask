41f3a3c331986645f5b33b70a77cd79d9bd79881
venkateswarara6 <104445024+venkateswararao10@users.noreply.github.com>
2023-03-17 11:42:54 +0000

    updated

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
+jobs=[
+  {'id':1,'title':'Data-scientist','location':'Bengaluru','salary':'Rs 20,00,000'},
+  {'id':2,'title':'Data-Analyst','location':'Delhi','salary':'Rs 10,00,000'},
+  {'id':3,'title':'Python Developer','location':'Hyderabad'}
+]
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html",jobs=jobs,company="Venky")
 if __name__=="__main__":
-   app.run(host='0.0.0.0',debug=True)
+   app.run( host='0.0.0.0',port=5000,debug=True)
