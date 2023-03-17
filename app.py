24c832f42f8fce6b5605b68b3bcc5f1334deca7a
venkateswarara6 <104445024+venkateswararao10@users.noreply.github.com>
2023-03-17 12:16:27 +0000

    modified

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("home.html",jobs=jobs,company="Venky")
+
+@app.route('/api/jobs')
+def joblist():
+  return jobs
 if __name__=="__main__":
    app.run( host='0.0.0.0',port=5000,debug=True)
